55b0aaa53f9d280e37a21a565a03403286d9ff1a
Vinay Singh <vinaysingh1028@gmail.com>
2020-05-14 01:52:27 +0530

    Even Odd Without using modulo

---
+++ EvenOddWithoutUsingModulo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter the number :\n");
+	scanf("%d",&num);
+	if(num & 1)
+	 printf("the number is Odd");
+	else
+	 printf("the number is Even");
+}
