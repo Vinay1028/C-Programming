5028e2dc534a7e16ff5a3144ef987ab33f0989a2
Vinay Singh <vinaysingh1028@gmail.com>
2020-05-14 01:53:42 +0530

    Program To Check Whether Given Number Is Even Or Odd

---
+++ ProgramToCheckWhetherGivenNumberIsEvenOrOdd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("Enter the number : \n");
+	scanf("%d",&number);
+	if(number%2 ==  0)
+	{
+		printf("number is even");
+	}
+	else
+	{
+		printf("The number is odd");
+	}
+}
