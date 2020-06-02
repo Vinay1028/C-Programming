33806c59d257b479926d0885969823feb0a7ea6e
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-02 16:47:10 +0530

    sun of digits of the number

---
+++ HackerRankCProgramming/SumOfDigitsOfAFiveDigitNumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    int sum=0;
+    int digit;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    if(n>=10000 && n <= 99999)
+    {
+        while(n>0)
+        {
+            digit = n%10;
+            sum += digit;
+            n /= 10;
+        }
+        printf("%d",sum);
+    }
+    return 0;
+}
+
+
